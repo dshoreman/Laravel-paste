6471058b2f0425085146da791212c205532aae46
Robin Malfait <malfait.robin@gmail.com>
2013-08-12 22:50:28 +0200

    Updated version 0.2.0

--- PasteLaravel.py
+++ PasteLaravel.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sublime, sublime_plugin, urllib
+import sublime, sublime_plugin, urllib, os
 try:
     from urllib.request import urlopen
     import urllib.parse
@@ -7,18 +7,35 @@ except ImportError:
     from urllib2 import urlopen
 
 class PastelaravelCommand(sublime_plugin.TextCommand):
-	def send_to_paste(self, body):
-		try:
-			data = urllib.urlencode( { 'paste' : body } ).encode('utf-8')
-		except AttributeError:
-			data = urllib.parse.urlencode( { 'paste' : body } ).encode('utf-8')
+    def send_to_paste(self, body):
+        try:
+            data = urllib.urlencode( { 'paste' : body } ).encode('utf-8')
+        except AttributeError:
+            data = urllib.parse.urlencode( { 'paste' : body } ).encode('utf-8')
 
-		http_file = urlopen('http://paste.laravel.com/', data)
-		return http_file
+        http_file = urlopen('http://paste.laravel.com/', data)
+        return http_file
 
-	def run(self, edit):
-		body = self.view.substr(self.view.sel()[0])
-		res = self.send_to_paste(body)
-		url = res.geturl()
-		sublime.set_clipboard(url)
-		sublime.status_message('Paste URL is in your clipboard: ' + url)
+    def get_text(self):
+        text = "";
+
+        for region in self.view.sel():
+
+            if not region.empty():
+
+                if text != "":
+                    text += os.linesep + os.linesep + os.linesep
+
+                text += self.view.substr(sublime.Region(region.begin(), region.end()))
+
+        if text != "":
+            return text
+        else:
+            return self.view.substr(sublime.Region(0, self.view.size()))
+
+    def run(self, edit):
+        body = self.get_text()
+        res = self.send_to_paste(body)
+        url = res.geturl()
+        sublime.set_clipboard(url)
+        sublime.status_message('Paste URL is in your clipboard: ' + url)
